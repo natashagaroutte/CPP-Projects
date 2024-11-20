a3d7e5e0ebbbcf460ca2933c94905391e6ef0ecc
Natasha Garoutte <natashag2600@gmail.com>
2024-11-20 14:21:35 -0600

    Create areaofcircle.cpp

---
+++ Programming I/Small Activities/Area of a Circle/areaofcircle.cpp
@@ -0,0 +1,32 @@
+//****************************************************************************************************
+//
+// Activity: Area of a Cirlce
+//
+// Objective: This activity is to teach the use of variables, initialization, constants, and
+//            statements.
+//
+// Prompt: Complete the following program such that it calculates the area of a circle whose
+//         radius is 7.757. Consider the value of pi to be 3.1415.
+//
+//****************************************************************************************************
+
+#include <iostream>
+using namespace std;
+
+//****************************************************************************************************
+
+int main ()
+{
+    double area; // variable definition
+    radius = 7.757; // initialization
+    const double PI = 3.1415; // constant
+
+    area = PI * (radius ^ 2); // statement to calculate area
+
+  cout << "Area is: " area;
+  return 0;
+}
+
+//****************************************************************************************************
+
+
