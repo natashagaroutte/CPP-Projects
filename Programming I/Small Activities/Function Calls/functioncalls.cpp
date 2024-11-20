d4376b447a2523e2bf8b9b26ecdc157f12b4e593
Natasha Garoutte <natashag2600@gmail.com>
2024-11-20 12:57:31 -0600

    Create functioncalls.cpp

---
+++ Programming I/Small Activities/Function Calls/functioncalls.cpp
@@ -0,0 +1,29 @@
+//****************************************************************************************************
+//
+// Activity: Function Calls
+//
+// Objective: This activity is to teach you the use of system defined function calls.
+//
+// Prompt: Complete the following program using pow() and sqrt() to calculate hypotenuse.
+//
+//****************************************************************************************************
+
+#include <iostream>
+#include <cmath>
+using namespacestd;
+
+int main ()
+{
+  double a,
+         b,
+         c;
+  cout << "Side a and b?" ;
+  cin >> a >> b;
+
+  c = sqrt(pow(a, 2.0) + (b, 2.0));
+
+  cout << endl << "Hypotenuse = " << c << endl;
+
+  return 0;
+
+}
