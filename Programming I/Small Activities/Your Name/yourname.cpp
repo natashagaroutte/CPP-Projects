5053b522e6e4960caaf39865964e681904ad6245
Natasha Garoutte <natashag2600@gmail.com>
2024-11-20 12:36:58 -0600

    Create yourname.cpp

---
+++ Programming I/Small Activities/Your Name/yourname.cpp
@@ -0,0 +1,10 @@
+//****************************************************************************************************
+// Activity: Your Name
+//
+// Objective: A function has three parts: prototype, definition, and calls. This activity 
+//            is to teach you the user defined function calls.
+//
+// Prompt: Write the main funciton to produce the output given under the sample output 
+//           using the functions whose prototypes are given.
+//
+//****************************************************************************************************
