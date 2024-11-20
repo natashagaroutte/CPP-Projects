c506be2a64fe3f2653eac640bca2837c2a2ae892
Natasha Garoutte <natashag2600@gmail.com>
2024-11-20 12:32:44 -0600

    Create maxofthree.cpp

---
+++ Programming I/Small Activities/Max of 3/maxofthree.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+//****************************************************************************************************
+
+void readNumbers(int &, int &, int &);
+int findMax(int, int, int);
+void displayMax(int);
+
+//****************************************************************************************************
+
+int main ()
+{
+  int num1,
+      num2,
+      num3,
+      max;
+
+readNumbers(num1, num2, num3);
+max = findMax(num1, num2, num3);
+diplayMax(max);
+
+//****************************************************************************************************
+
+void readNumbers(int number1, int number2, int number3)
+{
+  cout << "Enter three numbers: ";
+  cin >> number1 >> number2 >> number3;
+}
+
+//****************************************************************************************************
+
+int findMax(int num1, int num2, int num3)
+{
+  int max = num1;
+  if (max < num2)
+    max = num2;
+  if (max < num3)
+    max = num3;
+
+  return max;
+}
+
+//****************************************************************************************************
+
+void displayMax(int max)
+{
+  cout << "The max is: " << max << endl;
+}
+
+//****************************************************************************************************
+
+    
