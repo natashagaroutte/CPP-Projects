50ac427f3226b89ad820ca93ea9edb1ac27963a5
Natasha Garoutte <natashag2600@gmail.com>
2024-11-20 13:29:08 -0600

    Update yourname.cpp

--- Programming I/Small Activities/Your Name/yourname.cpp
+++ Programming I/Small Activities/Your Name/yourname.cpp
@@ -8,3 +8,87 @@
 //           using the functions whose prototypes are given.
 //
 //****************************************************************************************************
+
+#include <iostream>
+#include <string>
+
+using namespacestd;
+
+//****************************************************************************************************
+
+int main ()
+{
+
+  string fName,
+         mName,
+         lName;
+
+  fName = readFName();
+  mName = readMName();
+  lName = readLName();
+
+  displayName(fName);
+  displayStars();
+
+  displayName(mName);
+  displayStars();
+
+  displayName(lName);
+  displayStars();
+  
+  return 0;
+}
+
+//****************************************************************************************************
+
+string readFName()
+{
+  string fName;
+  cout << "What is your first name?";
+  cin >> fName;
+
+  return fName;
+}
+
+//****************************************************************************************************
+
+string readMName()
+{
+  string mName;
+  cout << "What is your middle name?";
+  cin >> mName;
+
+  return mName;
+}
+
+//****************************************************************************************************
+
+string readlName()
+{
+  string lName;
+  cout << "What is your last name?";
+  cin >> lName;
+
+  return lName;
+}
+
+//****************************************************************************************************
+
+void displayName(string name)
+{
+  cout << name;
+}
+
+//****************************************************************************************************
+
+void displayStars()
+{
+  cout << "**********";
+}
+
+//****************************************************************************************************
+
+
+
+
+
