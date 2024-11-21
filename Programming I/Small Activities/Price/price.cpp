6bf93b8146019af40ac031dc23702f96aa4d7c1c
Natasha Garoutte <natashag2600@gmail.com>
2024-11-20 21:44:31 -0600

    Create price.cpp

---
+++ Programming I/Small Activities/Price/price.cpp
@@ -0,0 +1,10 @@
+//****************************************************************************************************
+//
+// Activity: Price
+//
+// Objective: This activity is to teach the use of output Manipulation.
+//
+// Prompt: Write a program that generates a number between 0 and 60, inclusively and output it on 
+//         the screen. Note: each time the program runs, it should generate a different number.
+//
+//****************************************************************************************************
